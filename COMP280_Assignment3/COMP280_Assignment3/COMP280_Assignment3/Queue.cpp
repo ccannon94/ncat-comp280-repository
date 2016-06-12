eb08e7146b888269befe6df0ca91346d134c5416
Chris Cannon <chris.c.cannon@gmail.com>
2016-06-12 17:56:43 -0400

    Add comments to .cpp files

--- COMP280_Assignment3/COMP280_Assignment3/COMP280_Assignment3/Queue.cpp
+++ COMP280_Assignment3/COMP280_Assignment3/COMP280_Assignment3/Queue.cpp
@@ -7,6 +7,7 @@
 
 #include "Queue.h"
 
+// create an empty Queue
 Queue::Queue(){
     myfront = new qnode();
     myfront->data = NULL;
@@ -14,46 +15,69 @@ Queue::Queue(){
     myback = myfront;
 }
 
+//return true if Queue is empty, otherwise return false
 bool Queue::empty(){
     if(myfront->data == NULL)
         return true;
     return false;
 }
 
+//add a new value to the back of the Queue
 void Queue::AddQ(QueueElement x){
+    //if list isn't populated, assign this value to myfront
     if(empty()){
         myfront->data = x;
         myfront->next = myback;
-    }else if(myback->data == NULL){
+    }
+    //if myback isnt populated, assign this value to myback
+    else if(myback->data == NULL){
         myback->data = x;
-    }else{
+    }
+    //if fully populated, add it to the back
+    else{
+        //creates new node with given value
         qnode* newQ = new qnode();
         newQ->data = x;
+        //adds to the back
         myback->next=newQ;
+        //moves down back pointer
         myback=newQ;
     }
 }
 
+//retrieve the data at the front of the Queue
 int Queue::Front(QueueElement & x){
     return myfront->data;
 }
 
+//remove the value at the front of the Queue
 void Queue::RemoveQ(){
+    //if this is the last node, preserve it
     if(myfront->next == NULL){
+        //just set value to NULL
         myfront->data = NULL;
         myback = myfront;
-    }else{
+    }
+    //removes node completely
+    else{
+        //creates reference to front of the queue
         qnode* removed = myfront;
+        //moves the front pointer back
         myfront = myfront->next;
+        //deletes the referenced node
         delete removed;
     }
 }
 
+//displays the data stored in the Queue from front to back
 void Queue::display(){
     std::cout<<std::endl;
+    //first, check empty
     if(empty()){
         std::cout<<"This queue is empty."<<std::endl;
-    }else{
+    }
+    //if populated, increment and display each one
+    else{
         qnode* current = new qnode();
         current = myfront;
         std::cout<<current->data<<std::endl;
--- COMP280_Assignment3/COMP280_Assignment3/COMP280_Assignment3/Stack.cpp
+++ COMP280_Assignment3/COMP280_Assignment3/COMP280_Assignment3/Stack.cpp
@@ -7,50 +7,71 @@
 
 #include "Stack.h"
 
+// create an empty stack
 Stack::Stack(){
     mytop = new node();
     mytop->data = NULL;
     mytop->next = NULL;
 }
 
+//return true if stack is empty, otherwise return false
 bool Stack::empty(){
     if(mytop->data == NULL)
         return true;
     return false;
 }
 
+//add a new value to the top of the stack
 void Stack::push(StackElement x){
+    //if list is empty, I simply need to add the data to the top node
     if(empty()){
         mytop->data = x;
-    }else{
+    }
+    //for populated list, place this value on top and move up top pointer
+    else{
+        //creating pointer to top
         node* oldTop = mytop;
+        //creating new value
         node* newTop = new node();
         newTop->data = x;
+        //moving up the top
         newTop->next = oldTop;
         mytop = newTop;
     }
 }
 
+//retrieves the data that is at the top of the stack
 int Stack::Top(StackElement & x){
     return mytop->data;
 }
 
+//removes the value at the top of the stack
 void Stack::pop(){
+    //if this is the last node, preserve it
     if(mytop->next == NULL){
         mytop->data = NULL;
-    }else{
+    }
+    //delete unneeded nodes after removing value
+    else{
+        //create new node to reference value to be deleted
         node* popped = new node();
         popped = mytop;
+        //move down top pointer
         mytop = mytop->next;
+        //delete reference
         delete popped;
     }
 }
 
+//displays the data stored in the stack
 void Stack::display(){
     std::cout<<std::endl;
+    //first, check empty
     if(empty()){
         std::cout<<"The stack is empty."<<std::endl;
-    }else{
+    }
+    //if populated, increment and display each value
+    else{
         node* current = new node();
         current = mytop;
         std::cout<<current->data<<std::endl;
