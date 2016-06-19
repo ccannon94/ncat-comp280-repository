9e64cd3baa72ee6b878c4e282ff293b70c403812
Christopher Cannon <CCannon@macServer.c3technologies.xyz>
2016-06-19 17:27:10 -0400

    Create project for assignment 4

---
+++ COMP280_Assignment4/COMP280_Assignment4/COMP280_Assignment4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP280_Assignment4
+//
+//  Created by Christopher Cannon on 6/19/16.
+//  Copyright © 2016 Christopher Cannon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
