074add9b649285835cc031ef83405b3b75d9331e
Chris Cannon <chris.c.cannon@gmail.com>
2016-06-26 16:27:25 -0400

    Add comments

--- COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/Graph.cpp
+++ COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/Graph.cpp
@@ -10,8 +10,10 @@
 //initializes the number of verticies to v, and fills matrix with zeros
 Graph::Graph(int v)
 {
+    //intitializes number of verticies
     n = v;
     
+    //fills used portion of matrix to 0
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++)
             matrix[i][j] = 0;
@@ -21,40 +23,52 @@ Graph::Graph(int v)
 //add an edge between v and w
 void Graph::AddEdge(int v, int w)
 {
+    //sets edge from v to w to 1
     matrix[v][w] = 1;
+    //sets edge from w to v to 1
     matrix[w][v] = 1;
 }
 
 //delete the edge between v and w
 void Graph::DeleteEdge(int v, int w)
 {
+    //sets edge from v to w to 0
     matrix[v][w] = 0;
+    //sets edge from w to v to 0
     matrix[w][v] = 0;
 }
 
 //print all verticies that are adjacent to v
 int Graph::PrintAdjacent(int v)
 {
+    //creates an int to hold the output as a 32-bit int
     int output = 0;
+    //increments through the row associated with vertex v
     for(int i = 0; i < n; i++){
+        //if an edge exists, set a bit to 1 at that position and print a 1
         if(matrix[v][n] == 1){
             output += 1 << (n-i);
             std::cout<<1;
-        }else{
+        }
+        //otherwise, print a 0
+        else{
             std::cout<<0;
         }
     }
-        
+    
+    //return the 32-bit integer generated
     return output;
 }
 
 //print the content of the adjacency matrix with one row per line of output
 void Graph::PrintMatrix()
 {
+    //print the number showing existance of an edge at each position
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             std::cout<<matrix[i][j];
         }
+        //after a row is completed, start a new row on output terminal
         std::cout<<std::endl;
     }
 }
