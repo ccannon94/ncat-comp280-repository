ba523daae5de01d6b1fe0eae335765a4e90508d0
Chris Cannon <chris.c.cannon@gmail.com>
2016-06-26 15:35:38 -0400

    Initial commit for Assignment 5.
    Create program files.

---
+++ COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP280_Assignment5
+//
+//  Created by Christopher Cannon on 6/26/16.
+//  Copyright © 2016 Christopher Cannon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
