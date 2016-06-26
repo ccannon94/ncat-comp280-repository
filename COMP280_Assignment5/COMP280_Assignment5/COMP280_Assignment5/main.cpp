390ba3f64a6d16f898d7243cb6f047fc57d6a39f
Chris Cannon <chris.c.cannon@gmail.com>
2016-06-26 16:35:49 -0400

    Implement menu prompt in main.cpp

--- COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/main.cpp
+++ COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/main.cpp
@@ -7,8 +7,55 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    int exitCode = 0;
+    while(exitCode != 1){
+        int selection = 0;
+        std::cout<<"Undirected Graph Program"<<std::endl;
+        std::cout<<std::endl;
+        std::cout<<"Select an item from the menu below:"<<std::endl;
+        std::cout<<"1. Insert an edge."<<std::endl;
+        std::cout<<"2. Delete an edge."<<std::endl;
+        std::cout<<"3. Print adjacency matrix."<<std::endl;
+        std::cout<<"4. List all verticies that are adjacent to a specific vertex."<<std::endl;
+        std::cout<<"5. Print out verticies using depth first search."<<std::endl;
+        std::cout<<"6. Print out verticies using depth first search."<<std::endl;
+        std::cout<<"7. Check for connectivity."<<std::endl;
+        std::cout<<"8. Check for completeness."<<std::endl;
+        std::cout<<"9. Exit program."<<std::endl;
+        
+        std::cin>>selection;
+        
+        switch (selection) {
+            case 1:
+                //TODO option 1
+                break;
+            case 2:
+                //TODO option 2
+                break;
+            case 3:
+                //TODO option 3
+                break;
+            case 4:
+                //TODO option 4
+                break;
+            case 5:
+                //TODO option 5
+                break;
+            case 6:
+                //TODO option 6
+                break;
+            case 7:
+                //TODO option 7
+                break;
+            case 8:
+                //TODO option 8
+                break;
+            case 9:
+                exitCode = 1;
+                break;
+            default:
+                break;
+        }
+    }
 }
