2361943ad8c37a11ec2e6796592965e7c20a473d
Chris Cannon <chris.c.cannon@gmail.com>
2016-06-26 15:48:11 -0400

    Implement Graph.h from assignment.

--- COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/Graph.h
+++ COMP280_Assignment5/COMP280_Assignment5/COMP280_Assignment5/Graph.h
@@ -5,11 +5,20 @@
 //  Created by Christopher Cannon on 6/26/16.
 //
 
-#ifndef Graph_hpp
-#define Graph_hpp
-
 #include <stdio.h>
 
+const int MAX = 100;
 
-
-#endif /* Graph_hpp */
+class Graph
+{
+private:
+    int n; //number of verticies
+    int matrix[MAX][MAX]; //adjacency matrix
+    bool visited[MAX];
+public:
+    Graph(int v); //intitializes the number of verticies to v, and fills matrix with zeros
+    void AddEdge(int v, int w); //adds an edge between v and w
+    void DeleteEdge(int v, int w); //delete the edge betweeen v and w
+    int PrintAdjacent(int v); //print all verticies that are adjacent to v
+    void PrintMatrix(); //print the content of the adjacency matrix with one row per line of output
+};
